9ece80c7f53cdff1625516224f66ed4a7e149048
csdc24bb_horvatht <116713@fhwn.ac.at>
2024-01-19 20:57:05 +0100

    Edit Cors

--- src/main/java/com/backend/backend/controller/PublicApi.java
+++ src/main/java/com/backend/backend/controller/PublicApi.java
@@ -5,7 +5,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 
-@CrossOrigin(origins = "http://127.0.0.1/")
+@CrossOrigin(origins = "*")
 public interface PublicApi {
 
     @GetMapping(
